df9c32a2bb2bfd43a9b878459db91971328c2fbb
upiw256 <bilqimlb@gmail.com>
2022-11-03 21:32:28 +0700

    memisahkan setting

---
+++ component/Setting.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class Setting extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Setting</Text>
+      </View>
+    )
+  }
+}
